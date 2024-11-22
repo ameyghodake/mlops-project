f49ff37241e084851fe208d8025fa94a1450b34a
ameyghodake <ameyghodake8081@gmail.com>
2024-11-22 11:48:13 +0530

    FEAT: Evaluation and Prediction pipeline added

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -6,6 +6,7 @@ from ..entity.config_entity import (
     PrepareBaseModelConfig,
     PrepareCallbacksConfig,
     TrainingConfig,
+    EvaluationConfig,
 )
 
 
@@ -82,3 +83,13 @@ class ConfigurationManager:
             params_image_size=params.IMAGE_SIZE,
         )
         return training_config
+
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.keras",
+            training_data="artifacts/data_ingestion/test",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE,
+        )
+        return eval_config
